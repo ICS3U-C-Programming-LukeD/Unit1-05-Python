526a879c7467c1fa54acc0e96bc02d4323afd50b
Luke Di Bert <luke.di-bert@stu.ocsb.ca>
2025-02-20 04:41:47 +0000

    added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created By: Luke
+# Date: Feb 14, 2025
+# Perform basic math operators
+import math
+
+# Adds math module in order to perform square root calculations
+
+
+def main():
+    print("For a rectangle with a width of 10cm and a length of 6cm")
+    print()
+    print("The area is {}".format(6 * 10))
+    # Area calculation
+    print("the perimeter is {}".format(2 * 16))
+    # Perimeter calculation
+
+
+if __name__ == "__main__":
+    main()
